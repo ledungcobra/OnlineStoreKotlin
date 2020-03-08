c4d1ee03a70a18cb88757c62c8a334c0856f6ebe
ledungcobra <ledungcobra@gmail.com>
2020-03-09 00:51:02 +0700

    Login activity and sign up activity implemented

---
+++ app/src/main/java/com/example/onlinestorekotlin/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.onlinestorekotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        activity_main_btnSignUp.setOnClickListener {
+
+            startActivity(Intent(this@MainActivity, SignUpActivity::class.java))
+
+        }
+
+        activity_main_btnLogin.setOnClickListener {
+
+            val alertBuilder = AlertDialog.Builder(this)
+            alertBuilder.setTitle("Alert")
+            if (activity_main_edtLoginEmail.text.equals("") || activity_main_edtLoginPassword.text.equals(
+                    "")
+            ) {
+
+                alertBuilder.setMessage("Please check your email or password again")
+                alertBuilder.create().show()
+                return@setOnClickListener
+
+            }
+            val stringURL =
+                "http://192.168.1.7/OnlineStoreApp/login_user.php" + "?email=${activity_main_edtLoginEmail.text}&" +
+                        "password=${activity_main_edtLoginPassword.text}"
+            val requestQ = Volley.newRequestQueue(this)
+            val stringRequest = StringRequest(Request.Method.GET, stringURL, {
+
+                alertBuilder.setMessage(it.toString())
+                alertBuilder.create().show()
+                if(it.toString().equals("Login successfully")){
+                    finish()
+                    startActivity(Intent(this@MainActivity,HomeScreen::class.java))
+
+                }
+
+            }, {
+
+                alertBuilder.setMessage(it.toString())
+                alertBuilder.create().show()
+
+            })
+            requestQ.add(stringRequest)
+
+        }
+
+    }
+
+    fun hideKeyboard(v: View) {
+        v.hideKeyboard()
+
+    }
+
+}
+
---
+++ app/src/main/java/com/example/onlinestorekotlin/SignUpActivity.kt
@@ -0,0 +1,79 @@
+package com.example.onlinestorekotlin
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        sign_up_layout_btnLogin.setOnClickListener {
+            finish()
+        }
+
+        sign_up_layout_btnSignUp.setOnClickListener {
+            val alertBuilder = AlertDialog.Builder(this)
+            if(!sign_up_layout_edtCFPassword.text.toString()
+                    .equals(sign_up_layout_edtPassword.text.toString())){
+
+
+                alertBuilder.setTitle("Alert")
+                alertBuilder.setMessage("Password mismatch")
+                alertBuilder.create().show()
+
+            }else{
+
+
+                val serverURL = "http://192.168.1.7/OnlineStoreApp/join_new_user.php" +
+                        "?username=${sign_up_layout_edtUsername.text}&" +
+                        "email=${sign_up_layout_edtEmail.text}&" +
+                        "password=${sign_up_layout_edtPassword.text}"
+                val requestQ = Volley.newRequestQueue(this)
+                val stringRequest = StringRequest(Request.Method.GET,serverURL,{
+
+                    response->
+                    alertBuilder.setMessage(response.toString())
+
+                    alertBuilder.create().show()
+                    if(response.toString().equals("Sign Up successfully")){
+
+                        finish()
+                        startActivity(Intent(this@SignUpActivity,HomeScreen::class.java))
+
+                    }
+
+
+                },{
+                    error ->
+                    alertBuilder.setMessage(error.toString())
+                    alertBuilder.create().show()
+                })
+                requestQ.add(stringRequest)
+
+            }
+        }
+
+    }
+
+
+    fun hideKeyboard(v: View){
+        v.hideKeyboard()
+
+    }
+
+}
